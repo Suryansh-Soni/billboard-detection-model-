61807d8e1ccc5631aada4f8f5d1884d4b632173b
Suryansh Soni <suryanshsoni92@gmail.com>
2025-08-23 14:10:28 +0530

    reload=false

--- api.py
+++ api.py
@@ -66,4 +66,4 @@ async def predict(file: UploadFile = File(...)):
 if __name__ == "__main__":
     import uvicorn
     port = int(os.environ.get("PORT", 8000))
-    uvicorn.run("api:app", host="0.0.0.0", port=port, reload=True)
+    uvicorn.run("api:app", host="0.0.0.0", port=port, reload=False)
